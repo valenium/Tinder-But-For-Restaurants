f17328ff3957f756545fb5e1d4577085ec92afa7
Jeff Mancilla <jeffmancilla@outlook.com>
2023-11-21 14:36:11 -0600

    add models: User, Restaurant

--- app/models/Restaurant.js
+++ app/models/Restaurant.js
@@ -1,4 +1,34 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// module.exports = mongoose.model('', )
+const coordinateSchema = new Schema(
+	{
+		longitude: { type: Number },
+		latitude: { type: Number },
+	},
+	{ timestamps: true }
+)
+const locationSchema = new Schema(
+	{
+		city: { type: String },
+		zip_code: { type: Number },
+	},
+	{ timestamps: true }
+)
+
+const restaurantSchema = new Schema(
+	{
+		name: { type: String },
+        image_url: { type: String },
+        url: { type: String },
+		categories: [{ alias: String, title: String }],
+		coordinates: coordinateSchema,
+		price: { type: String, enum: ['$', '$$', '$$$', '$$$$'] },
+		location: locationSchema,
+        phone: { type: String },
+        distance: { type: Number },
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
--- app/models/User.js
+++ app/models/User.js
@@ -1,4 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-// module.exports = mongoose.model('', )
+const likeSchema = new Schema(
+	{
+		like: { type: Boolean },
+		restaurant: { type: Schema.Types.ObjectId, ref: 'Restaurant' },
+		comment: { type: String },
+	},
+	{ timestamps: true }
+)
+
+const userSchema = new Schema(
+	{
+		name: { type: String },
+		username: { type: String, unique: true },
+		zipCode: { type: Number },
+		city: {
+			type: String,
+			// enum ?
+		},
+		latitude: { type: Number },
+		longitude: { type: Number },
+		distance: { type: Number },
+		likes: [likeSchema],
+	},
+	{ timestamps: true }
+)
+
+module.exports = mongoose.model('User', userSchema)
