b5f1696c03a9e328c7974e58e443bf351eb631d3
valenium <valenium@live.com>
2023-11-22 12:27:30 -0600

    edit schema and create passport.js

--- app/server.js
+++ app/server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+// OAuth express session and passport modules
+const session = require('express-session')
+const passport = require('passport')
+
 const methodOverride = require('method-override')
+
 require('dotenv').config()
 require('./config/database')
+
+require('./config/passport')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,6 +30,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// OAuth middleware mount
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(methodOverride('_method'))
 
 app.use('/', indexRouter);
