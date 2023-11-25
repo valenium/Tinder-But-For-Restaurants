2cf680f2554e276243f6c447be003ffc9a1b1606
Jeff Mancilla <jeffmancilla@outlook.com>
2023-11-25 10:32:22 -0600

    complete filter template

--- app/routes/restaurants.js
+++ app/routes/restaurants.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const restaurantsCtrl = require ('../controllers/restaurants')
 
 /* GET users listing. */
+router.get('/restaurants/filter', restaurantsCtrl.filter);
+router.get('/restaurants/filter/:category', restaurantsCtrl.find);
 router.get('/restaurants/:id', restaurantsCtrl.show);
+
 
 module.exports = router;
