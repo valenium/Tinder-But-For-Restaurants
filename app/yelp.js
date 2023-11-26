4534af2db17133df86c3f8cd8481ae9bb39f37fa
Jeff Mancilla <jeffmancilla@outlook.com>
2023-11-26 12:57:58 -0600

    switch filter controller over to use yelp api module, insert results to restaurants collection

--- app/yelp.js
+++ app/yelp.js
@@ -1,25 +1,7 @@
 require('dotenv').config()
 
-module.exports = {
-	getBusinesses: function (location, radius) {
-		getYelpData(businessesUrl,'', businessesParams(location, radius))
-	},
-	getReviews: function (businessId) {
-		getYelpData(reviewsUrl, businessId, reviewsQueries)
-	},
-}
-const businessesUrl = 'https://api.yelp.com/v3/businesses/search'
-const reviewsUrl = 'https://api.yelp.com/v3/businesses/'
-const reviewsQueries = '/reviews?limit=20&sort_by=yelp_sort'
-
-function convertToMeters(miles) {
-	return miles * 1609.344
-}
-
-function businessesParams(location, radius) {
-    radius = convertToMeters(radius)
-	return `location=${location}&radius=${radius}&sort_by=best_match&limit=50`
-}
+const baseUrl = 'https://api.yelp.com/v3'
+const reviewsQueries = '?limit=3&sort_by=yelp_sort'
 
 async function getYelpData(url, pathParams, queryParams) {
 	try {
@@ -28,12 +10,40 @@ async function getYelpData(url, pathParams, queryParams) {
 				accept: 'application/json',
 				Authorization: `Bearer ${process.env.YELP_API_KEY}`,
 			},
-			body: JSON.stringify(data),
 		})
-		return response.status !== 200
-			? console.error(`HTTP ${response.status}: `, response.body)
-			: response.json()
+		if (response.status !== 200) {
+			console.info(`HTTP status ${response.status}: `, await response.json())
+		} else {
+			return await response.json()
+		}
 	} catch (err) {
 		console.error('Yelp API Error: ', err)
 	}
+}
+
+function convertToMeters(miles) {
+	return parseInt(miles * 1609.344)
+}
+
+function businessesParams(location, radius) {
+	radius = convertToMeters(radius)
+	return `?location=${location}&radius=${radius}&sort_by=best_match&limit=20` //
+}
+
+module.exports = {
+	getBusinesses: function (location, radius) {
+		return getYelpData(
+			baseUrl,
+			'/businesses/search',
+			businessesParams(location, radius)
+		)
+	},
+	getReviews: function (businessId) {
+		return getYelpData(
+			baseUrl,
+			`/businesses/${businessId}/reviews`,
+			reviewsQueries
+		)
+	},
+	convertToMeters,
 }
