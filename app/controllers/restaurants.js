d2b51b81868796b2af5441c9e5673710e707922b
Jeff Mancilla <jeffmancilla@outlook.com>
2023-11-27 17:35:49 -0600

    controller rewrite start progress, switching IDEs

--- app/controllers/restaurants.js
+++ app/controllers/restaurants.js
@@ -1,6 +1,5 @@
 const Restaurant = require('../models/Restaurant')
 const User = require('../models/User')
-const yelp = require('../api/yelp')
 
 module.exports = {
 	show,
@@ -15,24 +14,40 @@ async function show(req, res) {
 }
 
 async function filter(req, res) {
-	console.log(req.user)
-	const yelpData = await yelp.getBusinesses(req.user.zipCode, req.user.distance)
-	const nearbyRestaurants = await Restaurant.insertMany(yelpData.businesses)
+	const url = `https://api.yelp.com/v3/businesses/search?location=${req.user.zipCode}&radius=40000&sort_by=best_match&limit=50`
+	try {
+		const response = await fetch(url, {
+			headers: {
+				accept: 'application/json',
+				Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+			},
+		})
+		if (response.status !== 200) {
+			throw new Error(`HTTP status ${response.status}: `, await response.json())
+		} else {
+			const yelpData = await response.json()
+			const { id, name, image_url, url, categories, price, location, phone } = yelpData.businesses
+			// filter to only insert
+			const nearbyRestaurants = await Restaurant.insertMany(yelpData.businesses)
 
-	let categorySet = new Set(
-		nearbyRestaurants
-			.map((restaurant) =>
-				restaurant.categories.map((category) => category.title)
+			let categorySet = new Set(
+				nearbyRestaurants
+					.map((restaurant) =>
+						restaurant.categories.map((category) => category.title)
+					)
+					.flat()
 			)
-			.flat()
-	)
-	res.render('restaurants/filter', {
-		title: 'Restaurant Filters',
-		categories: categorySet,
-	})
+			res.render('restaurants/filter', {
+				title: 'Restaurant Filters',
+				categories: categorySet,
+			})
+		}
+	} catch (err) {
+		console.error('Yelp API Error: ', err)
+		res.send(err)
+	}
 }
 async function find(req, res) {
-
 	let queryObj = {}
 	// req.body way - delete me later
 	// queryObj['categories.title'] = req.body.category
