843923671f293b7e92275df4059431883395851d
valenium <valenium@live.com>
2023-11-29 09:06:47 -0600

    change oauth new user flow

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -14,7 +14,7 @@ module.exports = {
 async function newUser(req, res) {
 	const user = await User.findById(req.params.id)
 	try {
-		res.render('users/edit', {
+		res.render('users/new', {
 			title: 'New User',
 			errorMsg: '',
 			user,
@@ -70,10 +70,18 @@ async function edit(req, res) {
 
 async function update(req, res) {
 	try {
-		await User.findById(req.params.id)
-		await User.findOne(req.body)
+		let newUser = false
+		let userId = await User.findById(req.params.id)
+		console.log(userId)
+		if (!userId.zipCode) {
+			newUser = true
+		}
 		await User.updateOne({ _id: req.params.id }, { $set: req.body })
+		if (newUser === true){
+			res.redirect('/restaurants/filter')
+		} else {
 		res.redirect(`/users/${req.params.id}`)
+		}
 	} catch (err) {
 		console.log(err)
 	}
