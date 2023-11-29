466f17e3249465e25f295b351b7d1b0980dfc709
valenium <valenium@live.com>
2023-11-28 21:58:21 -0600

    fix left panel on homepage

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -81,6 +81,7 @@ async function update(req, res) {
 
 async function deleteUser(req, res) {
 	try {
+        // await User.findByIdAndUpdate(req.params.id, { googleId: null })
 		await User.findByIdAndDelete(req.params.id)
 		res.redirect('/')
 	} catch (err) {
